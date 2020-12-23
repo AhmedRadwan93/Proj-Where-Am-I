4cd9dc3c2468f0a9cd4115dcb3a44dfdb5e0bdcd
AhmedRadwan93 <ahmedelmaghwry93@gmail.com>
2020-12-23 08:43:00 +0000

    pushing the project after modification

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/where-am-I/src
+# Build directory: /home/workspace/where-am-I/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(my_robot)
